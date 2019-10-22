2350e3d84185a0e3d182ab08470d28f7e1d8c010
Rodrigo Cichetto <rodrigocichetto@gmail.com>
2019-10-22 13:43:55 -0300

    add students

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -21,7 +21,7 @@ class UserController {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
-      return res.status(400).json({ error: 'User already exists.' });
+      return res.status(400).json({ error: 'User already exists' });
     }
 
     const { id, name, email, provider } = await User.create(req.body);
@@ -61,7 +61,7 @@ class UserController {
       const userExists = await User.findOne({ where: { email } });
 
       if (userExists) {
-        return res.status(400).json({ error: 'User already exists.' });
+        return res.status(400).json({ error: 'User already exists' });
       }
     }
 
